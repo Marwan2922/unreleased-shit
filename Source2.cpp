95fd3d5db2032996297038ef42e70f88d76f00b1
Marwan_Hegazy_ <106491308+Marwan2922@users.noreply.github.com>
2025-09-27 01:20:25 +0300

    Add files via upload

---
+++ Source2.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+#include "Image_Class.h"
+
+int main() {
+
+    string filename;
+    int x, y, w, h;
+    cout << "Pls enter image name to crop it: ";
+    cin >> filename;
+    cout << "Pls enter starting point: ";
+    cin >> x >> y;
+    cout << "please enter the dimensions of the area to cut: ";
+    cin >> w >> h;
+
+    Image image(filename);
+
+
+    if (x + w > image.width || y + h > image.height)
+    {
+        cout << "Error ";
+    }
+    else {
+        Image cropped(w, h);
+
+        for (int i = 0; i < w; ++i) {
+            for (int j = 0; j < h; ++j) {
+
+                for (int k = 0; k < 3; ++k) {
+
+
+
+
+
+                    cropped(i, j, k) = image(i + x, j + y, k);
+
+                }
+
+            }
+
+
+        }
+
+
+        cout << "Pls enter image name to store new image\n";
+        cout << "and specify extension .jpg, .bmp, .png, .tga: ";
+
+        cin >> filename;
+        cropped.saveImage(filename);
+        system(filename.c_str());   //
+
+    }
+}
